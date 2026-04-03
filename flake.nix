35f289222ccadfc78e9419d3e7dd639935c17861
rileycat <deliciousfaith@live.net>
2026-04-03 09:44:37 -0700

    one more cleanup; update to 25.11

--- flake.nix
+++ flake.nix
@@ -1,30 +1,39 @@
 {
-  description = "A very basic flake";
+  description = ''
+    A simple flake.
+
+    This flake shows off several things beginners want to do, such as having
+    unstable and stable nixpkgs, usage of home manager, and importing
+    modules from another flake into the configuration.
+   '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    catppuccin.url = "github:catppuccin/nix/release-25.05";
+    
+    catppuccin.url = "github:catppuccin/nix/release-25.11";
   };
 
-  outputs = { self, nixpkgs, unstable, home-manager, catppuccin }@inputs:
+  outputs = { self, nixpkgs, unstable, home-manager, catppuccin }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     pkgs-unstable = unstable.legacyPackages.${system};
   in {
+    ## nixos configuration
     nixosConfigurations.blackjack = nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit pkgs-unstable; }; 
       modules = [
         ./configuration.nix
-
         catppuccin.nixosModules.catppuccin
 
+        ## home manager nixos module
         # home-manager.nixosModules.home-manager
         # {
         #   home-manager.useGlobalPkgs = true;
@@ -36,7 +45,8 @@
         # }
       ];
     };
-    
+
+    ## home manager standalone configuration
     homeConfigurations."rileycat" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
       modules = [
